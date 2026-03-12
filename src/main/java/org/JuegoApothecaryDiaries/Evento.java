41633e6d042f843c781233a98d66690f8232ad84
Ravenant1234 <69699906+Ravenant1234@users.noreply.github.com>
2026-03-12 12:59:59 -0300

    Enhance Evento class with description and participants
    Updated the Evento class to include a description and participants set. Modified constructor and added methods for managing participants.

--- src/main/java/org/JuegoApothecaryDiaries/Evento.java
+++ src/main/java/org/JuegoApothecaryDiaries/Evento.java
@@ -1,14 +1,20 @@
 public class Evento{
+    //cada tipo de evento, por ejemplo "Quest", "Hallazgo" tiene un id que lo identifica y una descripcion y los "participantes" como una referencia a objetos
+    
     private enum Tipo{
         HALLAZGO_ITEM, DIALOGO, ENEMIGO, COMBATE, VIAJE 
     }
 
     private boolean TRIGGERED;
     private Tipo tipoEvento;
+    private String descripcion;
+    private Set<Personaje> participantes;
   
-    public Evento (Tipo tipoEvento){
+    public Evento (Tipo tipoEvento, String descripcion, Set<Personaje> participantes){
         TRIGGERED = FALSE;
         this.tipoEvento = tipoEvento;
+        this.descripcion = descripcion;
+        this.participantes = participantes;
     }
 
     public void activarEvento (){
@@ -19,12 +25,28 @@ public class Evento{
         TRIGGERED = FALSE;
     }
 
+    public void agregarParticipante (Participante p){
+        participantes.insert (p);
+    }
+
     public boolean estaActivado (){
         return TRIGGERED;
     }
 
     public enum tipoEvento (){
-        return tipoEvento
+        return tipoEvento;
     }
 
+    public String getDescripcion (){
+        return descripcion;
+    }
+
+    public Set<Personaje> getParticipantes (){
+        return participantes;
+    }
+
+    public boolean estaParticipante (Participante p){
+        return participantes.get (p);
+    }
+    
 }
