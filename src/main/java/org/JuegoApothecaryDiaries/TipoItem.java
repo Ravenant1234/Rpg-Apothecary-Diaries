2ce90b5cea64890c9178febe9ed520ae17d491eb
Ravenant1234 <69699906+Ravenant1234@users.noreply.github.com>
2026-03-12 15:34:12 -0300

    Delete src/main/java/org/JuegoApothecaryDiaries/TipoItem.java

--- src/main/java/org/JuegoApothecaryDiaries/TipoItem.java
+++
@@ -1,4 +0,0 @@
-enum TipoItem{
-    // una herramienta puede ser un repuesto de una maquina o una herramienta concreta que se necesita para una quest, o para darselo a un npc
-    POCION, PLANTA, HERRAMIENTA
-}
