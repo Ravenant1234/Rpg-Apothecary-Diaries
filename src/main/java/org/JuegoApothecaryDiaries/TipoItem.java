b57733f9b852f5eea6f460df8e06acd25f81e312
Ravenant1234 <69699906+Ravenant1234@users.noreply.github.com>
2026-03-12 13:15:03 -0300

    Add comment to TipoItem enum for clarity
    Added comment to explain the purpose of the enum values.

---
+++ src/main/java/org/JuegoApothecaryDiaries/TipoItem.java
@@ -0,0 +1,4 @@
+enum TipoItem{
+    // una herramienta puede ser un repuesto de una maquina o una herramienta concreta que se necesita para una quest, o para darselo a un npc
+    POCION, PLANTA, HERRAMIENTA
+}
